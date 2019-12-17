9cd37f195d720a8a19190984572f809e708389c2
philenius <philenius@users.noreply.github.com>
2019-12-17 02:59:09 +0100

    add HTML layout with buttons for setting health

--- golang-dummy-web-service/app.go
+++ golang-dummy-web-service/app.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"os"
@@ -12,17 +15,21 @@ import (
 )
 
 var (
+	healthy 		 = true
+	port			 = "8081"
 	shutdownOngoing  = false
 	remainingSeconds = 10
 )
 
 func main() {
 
-	port := getEnv("SERVER_PORT", "8081")
+	port = getEnv("SERVER_PORT", "8081")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler)
+	router.HandleFunc("/info", infoHandler)
 	router.HandleFunc("/health", healthHandler)
+	router.HandleFunc("/health/{healthy}", setHealthHandler)
 	router.HandleFunc("/shutdown", shutdownHandler)
 	http.Handle("/", router)
 	log.Println("listening on :" + port)
@@ -37,7 +44,27 @@ func getEnv(key string, fallback string) string {
 }
 
 func homeHandler(writer http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	tmpl.Execute(writer, nil)
+}
 
+func setHealthHandler(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	healthQueryParam, err := strconv.ParseBool(vars["healthy"])
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write([]byte("BAD REQUEST"))
+		return	
+	}
+
+	healthy = healthQueryParam
+	log.Printf("GET /healthy/%t", healthy)
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
+}
+
+func infoHandler(writer http.ResponseWriter, request *http.Request){
 	headerMap := request.Header
 	acc := headerMap["Accept"]
 	enc := headerMap["Accept-Encoding"]
@@ -48,22 +75,32 @@ func homeHandler(writer http.ResponseWriter, request *http.Request) {
 	pragma := headerMap["Pragma"]
 	userAgent := headerMap["User-Agent"]
 	upgradeSSL := headerMap["Upgrade-Insecure-Requests"]
-	log.Println("received request", acc, enc, lang, cache, con, host, pragma, upgradeSSL, userAgent)
+	log.Println("GET /info\t\t", acc, enc, lang, cache, con, host, pragma, upgradeSSL, userAgent)
 
 	hostname, err := os.Hostname()
 	if err != nil {
 		log.Println("failed to retrieve hostname", err.Error())
 	}
+	headerMapJSON, err := json.Marshal(headerMap)
 	writer.Header().Set("Content-Type", "application/json")
-	writer.Write([]byte(fmt.Sprintf("{ \"hostname\": \"%s\", \"MY_ENV_VAR\": \"%s\" }\n", hostname, os.Getenv("MY_ENV_VAR"))))
+	writer.Write([]byte(fmt.Sprintf("{ \"hostname\": \"%s\", \"SERVER_PORT\": %s, \"MY_ENV_VAR\": \"%s\", \"headerMap\": %s }\n", hostname, port, os.Getenv("MY_ENV_VAR"), headerMapJSON)))
 }
 
 func healthHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("OK"))
+	log.Println("GET /health\t\thealthy =", healthy)
+
+	writer.Header().Add("Content-Type", "application/json")
+	if healthy {
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("{ \"healthy\": true }"))
+		return	
+	}
+	writer.WriteHeader(http.StatusInternalServerError)
+	writer.Write([]byte("{ \"healthy\": false }"))
 }
 
 func shutdownHandler(writer http.ResponseWriter, request *http.Request) {
+	log.Println("GET /shutdown")
 	writer.WriteHeader(http.StatusOK)
 
 	if shutdownOngoing {
